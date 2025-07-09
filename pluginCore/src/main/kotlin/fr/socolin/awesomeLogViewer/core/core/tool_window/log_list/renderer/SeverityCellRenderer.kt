f462ef3cc32c194bbf8ac5cc800cf38c91f52476
Socolin <provost.bertrand@gmail.com>
2025-07-09 16:26:14 -0400

    Add small margin on the severity column

--- pluginCore/src/main/kotlin/fr/socolin/awesomeLogViewer/core/core/tool_window/log_list/renderer/SeverityCellRenderer.kt
+++ pluginCore/src/main/kotlin/fr/socolin/awesomeLogViewer/core/core/tool_window/log_list/renderer/SeverityCellRenderer.kt
@@ -1,15 +1,15 @@
 package fr.socolin.awesomeLogViewer.core.core.tool_window.log_list.renderer
 
 import com.intellij.openapi.editor.ex.util.EditorUtil
-import com.intellij.ui.JBColor
+import com.intellij.util.ui.JBUI
 import fr.socolin.awesomeLogViewer.core.core.session.LogEntryDisplay
 import fr.socolin.awesomeLogViewer.core.core.session.LogSession
 import fr.socolin.awesomeLogViewer.core.core.utilities.GenericSeverityLevel
-import org.apache.velocity.runtime.directive.contrib.For
 import java.awt.Component
 import javax.swing.JLabel
 import javax.swing.JTable
 import javax.swing.table.TableCellRenderer
+
 
 class SeverityCellRenderer(
     val logSession: LogSession
@@ -40,6 +40,7 @@ class SeverityCellRenderer(
     fun setValue(logEntryDisplay: LogEntryDisplay) {
         renderModel.reset()
         font = EditorUtil.getEditorFont()
+        border = JBUI.Borders.emptyLeft(5)
 
         logEntryDisplay.updateSeverityRenderModel(logSession, renderModel)
         if (renderModel.text != null)
